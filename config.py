79adeab0997d1de28009d3b4fd2439b2e70a46c6
lumonovskiy <94923011+VodaUK@users.noreply.github.com>
2025-01-25 15:18:15 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,10 @@ import os
 
 api_id = os.environ.get("API_ID")
 api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("BOT_TOKEN")
 
 user_to_notify_username = "@rodionmurzo"
-
-channels_to_track = [
-    "@exposergmd",
-    "@testnorg",
-]
-
+channels_to_track = ["@exposergmd" "@testnorg"]
 notification_text = "{post_link}"
+
+admin_usernames = ["lumohn"]
