db0abe5596cb31f383788fb6608cb815075c2695
lumonovskiy <94923011+VodaUK@users.noreply.github.com>
2025-01-25 13:02:16 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,12 @@
-# config.py
+import os
 
-# *** Замените на свои значения после получения с my.telegram.org/apps ***
-api_id = "YOUR_API_ID"
-api_hash = "YOUR_API_HASH"
+api_id = os.environ.get("API_ID")
+api_hash = os.environ.get("API_HASH")
 
-# *** ID или username пользователя, которому будут отправляться уведомления ***
-user_to_notify_username = "@username_получателя"  # Или user_to_notify_id = 123456789
+user_to_notify_username = "@username_получателя"
 
-# *** Список каналов для отслеживания (username или ID) ***
 channels_to_track = [
-    "название_канала_1",
-    "название_канала_2",
-    # Можно добавить еще каналы
+    "@exposergmd",
 ]
 
-# *** Текст уведомления ***
 notification_text = "Новый пост в канале {channel_name}:\n\n{post_text}\n\nСсылка: {post_link}"
