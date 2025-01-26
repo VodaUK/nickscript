e0b003f0832f8ae939f715faea07185e91fdcdc6
lumonovskiy <94923011+VodaUK@users.noreply.github.com>
2025-01-26 14:58:51 +0500

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-api_id = 20115593
-api_hash = 'd80553a3842de01257fe6bc570116c88'
-bot_token = '7535013553:AAGLMtFHqUJMnYSZRLxZ1oLreZIvhQqGKtw'
-notify_users_usernames = [""]
-channels_to_track = [""]
-notification_text = ""
-admin_usernames = ["lumohn", "nickxd"]
